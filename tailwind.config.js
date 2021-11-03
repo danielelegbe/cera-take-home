8ab120ac959112c10f6137f76b868445c0687e3e
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-11-03 11:59:32 +0000

    add primary colour and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      primary: '#880E4F',
+    }),
+    extend: {
+      fontFamily: {
+        body: ['Cairo'],
+      },
+    },
   },
   variants: {
     extend: {},
