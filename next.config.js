3c692171abe4603b53fd8abd8e9e86eed2f653a9
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2021-11-03 15:25:15 +0000

    add domains for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['ceracare.co.uk', 'www.ceracare.co.uk', 'images.unsplash.com'],
+  },
+};
